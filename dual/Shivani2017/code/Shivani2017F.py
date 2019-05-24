4fefc3d8b17be25f93c35ea71e4e99e1bad3cd2f
Ernesto Avila Domenech <eadomenech@gmail.com>
2019-05-24 10:29:39 -0400

    Arquitectura inicial para Shivani2017

--- dual/Shivani2017/code/Shivani2017F.py
+++ dual/Shivani2017/code/Shivani2017F.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-from helpers.blocks_class import BlocksImage
-from helpers import utils, progress_bar
+from block_tools.blocks_class import BlocksImage
 from image_tools.ImageTools import ImageTools
 from PIL import Image
 from scipy import misc
@@ -8,6 +7,7 @@ import numpy as np
 import pywt
 import cv2
 import math
+import random
 
 
 class Shivani2017F():
@@ -16,118 +16,71 @@ class Shivani2017F():
     embed fragile watermark into the host image.
     """
 
-    def __init__(self):
+    def __init__(self, k1='password1', k2='password2'):
 
-        # Cargando watermark
-        watermark = Image.open("static/Watermarking.png").convert("1")
+        # Random vectors
+        self.R1 = []
+        self.R2 = []
+    
+    def generateR1andR2(self, M, N):
+        for i in range(M):
+            self.R1.append(random.randint(0, 1))
+        for i in range(N):
+            self.R2.append(random.randint(0, 1))
 
-        # Obteniendo array de la watermark
-        watermark_array = np.asarray(watermark)
-
-        # Datos de la watermark como lista
-        watermark_as_list = watermark_array.reshape(
-            (1, watermark_array.size))[0]
+    def insertarEnComponente(self, component_image):
+        '''Insertar en una componente'''
         
-        # Tomando solo los valores correspondientes a los datos
-        self.watermark_list = []
-        for p in watermark_as_list:
-            if p:
-                self.watermark_list.append(255)
-            else:
-                self.watermark_list.append(0)
+        return component_image
         
-        # Calculando e imprimeindo datos iniciales
-        self.len_watermark_list = len(self.watermark_list)
-
-        self.cant_posibilidades = None
-        self.v = []
 
     def insert(self, cover_image):
-        # Instancias necesarias
-        itools = ImageTools()
-
-        # Embedding of Copyright Information (robust watermarking)
-        # Convirtiendo a modelo de color YCbCr
-        cover_ycbcr_array = itools.rgb2ycbcr(cover_image)
-
-        # Tomando componente Y
-        cover_array = cover_ycbcr_array[:, :, 0]
-
-        # Wavelet Transform
-        LL, [LH, HL, HH] = pywt.dwt2(cover_array, 'haar')
-
-        # Simulando pwlcm para obtener las posiciones a utilizar
-        import random
-        self.cant_posibilidades = LL.size
-
-        colums = len(LL[0])
-
-        while len(self.v) < self.len_watermark_list:
-            val = random.randrange(self.cant_posibilidades)
-            if val not in self.v:
-                self.v.append(val)
+        # Generate R1 and R2
+        m, n = cover_image.size
+        self.generateR1andR2(m, n)
         
-        # Embedding
-        for i in range(self.len_watermark_list):
-            px = self.v[i] // colums
-            py = self.v[i] - (px * colums)
-            if self.watermark_list[i] == 255:
-                if HL[px, py] <= LH[px, py]:
-                    T = abs(LH[px, py])-abs(HL[px, py])
-                    A3w = T + HL[px][py]
-                    HL[px, py] = A3w + (LH[px, py]+HL[px, py])/2.0
-            else:
-                if LH[px, py] <= HL[px, py]:
-                    T = abs(HL[px, py])-abs(LH[px, py])
-                    A2w = T + LH[px, py]
-                    LH[px, py] = A2w + (LH[px, py]+HL[px, py])/2.0
+        # Dividiendo en componentes RGB
+        r, g, b = cover_image.split()
         
-        # Inverse transform
-        cover_ycbcr_array[:, :, 0] = pywt.idwt2((LL, (LH, HL, HH)), 'haar')
+        # Marcando cada componente
+        r = self.insertarEnComponente(r)
+        g = self.insertarEnComponente(g)
+        b = self.insertarEnComponente(b)
 
-        image_rgb_array = itools.ycbcr2rgb(cover_ycbcr_array)
+        return Image.merge("RGB", (r, g, b))
+    
+    def extractEnComponente(self, component_image):
+        '''Insertar en una componente'''
+
+        pass                   
         
-        # Generating the watermarked image
-        watermarked_image = Image.fromarray(image_rgb_array)
-
-        return watermarked_image
-
+        return []
+    
     def extract(self, watermarked_image):
-        # Instancias necesarias
-        itools = ImageTools()
-
-        # Embedding of Copyright Information (robust watermarking)
-        # Convirtiendo a modelo de color YCbCr
-        watermarked_ycbcr_array = itools.rgb2ycbcr(watermarked_image)
-
-        # Tomando componente Y
-        watermarked_array = watermarked_ycbcr_array[:, :, 0]
-
-        # Wavelet Transform
-        LL, [LH, HL, HH] = pywt.dwt2(watermarked_array, 'haar')
-
-        colums = len(LL[0])
-
-        extract = []
+        '''
+        Retorna la imagen marcada con los pixeles modificados de
+        color verde
+        '''
+        # Dividiendo en componentes RGB
+        r, g, b = watermarked_image.split()
         
-        # Extracting
-        for i in range(self.len_watermark_list):
-            px = self.v[i] // colums
-            py = self.v[i] - (px * colums)
-            if HL[px, py] >= LH[px, py]:
-                extract.append(1)
-            else:
-                extract.append(0)
-        
-        wh = int(math.sqrt(self.len_watermark_list))
-        extract_image1 = Image.new("1", (wh, wh), 255)
-        array_extract_image = misc.fromimage(extract_image1)
+        # Bloques modificados en componente Red
+        bmr = self.extractEnComponente(r)
+        # Bloques modificados en componente Green
+        bmg = self.extractEnComponente(g)
+        # Bloques modificados en componente Blue
+        bmb = self.extractEnComponente(b)
 
-        for i in range(wh):
-            for y in range(wh):
-                if extract[wh*i+y] == 0:
-                    array_extract_image[i, y] = 0
+        bm = list(set(bmr+bmg+bmb))
 
-        watermark_extracted = misc.toimage(array_extract_image)        
+        # Dividing in 1x1 blocks
+        watermarked_array = misc.fromimage(watermarked_image)
+        blocks = BlocksImage(watermarked_array, 1, 1)        
         
-        return watermark_extracted
+        for item in bm:
+            coord = blocks.get_coord(item)
+            cv2.rectangle(
+                watermarked_array, (coord[1], coord[0]),
+                (coord[3], coord[2]), (0, 255, 0), 1)
+
+        return misc.toimage(watermarked_array)
